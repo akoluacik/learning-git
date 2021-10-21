0fc00f33205c153ced8cb49814ef812cb37bad2f
akoluacik <79508091+akoluacik@users.noreply.github.com>
2021-10-21 11:57:24 +0300

    Update test.c
    greetings function is added.

--- test.c
+++ test.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+void greetings(char []);
+
 int main(){
 	printf("%s\n", "Hello, World!");
+	greetings("C");
 	return 0;
+}
+
+void greetings(char name[]){
+	printf("%s%s\n", "Hello, ", name);
 }
